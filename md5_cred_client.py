d6900769cc3a6654fa14e74404226cc7e0be4771
Jerin John <jerin.j@somaiya.edu>
2017-08-12 21:13:46 +0530

    MD5 Authentication completed.

--- md5_cred_client.py
+++ md5_cred_client.py
@@ -1,15 +1,40 @@
 import hashlib
 import socket
 
-md5_hasher = hashlib.md5()
+# Configure client socket and connection info
+client_skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = socket.gethostname()
+port = 9000
+mode = "auth"
 
-print("MD5 Login Credential Authenticator")
+print("\nMD5 Login Credential Authenticator\n")
 
 username = input("Enter username : ")
-passwd = input("Enter password : ")
+password = input("Enter password : ")
 
-md5_hasher.update(username.encode('utf-8'))
-user_hash = md5_hasher.hexdigest()
+if username == "admin" and password == "admin":
+	mode = "admin"
+	print("\nNew user registration :-")
+	username = input("Enter username : ")
+	password = input("Enter password : ")
 
-md5_hasher.update(username.encode('utf-8'))
-pass_hash = md5_hasher.hexdigest()
+# Obtain md5 hash checksum for credentials
+mode_hash = hashlib.md5(mode.encode()).hexdigest()
+
+user_hash = hashlib.md5(username.encode()).hexdigest()
+
+pass_hash = hashlib.md5(password.encode()).hexdigest()
+
+# Connect to authentication server
+client_skt.connect((host, port))
+payload = mode_hash + "/" + user_hash + "/" + pass_hash
+
+# print(payload)
+
+# Send auth request to server with credentials
+client_skt.send(payload.encode())
+
+print("\n" + client_skt.recv(9999).decode())
+
+print("Closing connection. Server out.")
+client_skt.close()
--- md5_cred_server.py
+++ md5_cred_server.py
@@ -1,8 +1,50 @@
 import hashlib
+import pickle
+import socket
 
-md5_hasher = hashlib.md5()
+# Configure client socket and connection info
+server_skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = socket.gethostname()
+port = 9000
+auth_status = ''
 
-with open('cred_crypt.txt', 'rb') as cred_file:
-	buf = cred_file.read()
-	md5_hasher.update(buf)
-print(md5_hasher.hexdigest())
+# Connect to port and listen to client requests
+server_skt.bind((host, port))
+server_skt.listen(5)
+print("\nAuthentication Server running on port :", port)
+
+# Accept and process client requests
+conn, (ip, cport) = server_skt.accept()
+auth_req = conn.recv(port).decode()
+mode, uname, passwd = auth_req.split('/')
+# print(mode, uname, passwd)
+
+# Open credentials hash file
+auth_creds = pickle.load(open('auth_cred.p', 'rb'))
+
+# Check for authentication mode
+if mode == hashlib.md5("admin".encode()).hexdigest():
+	# Add user in admin mode
+	if uname not in auth_creds.keys():
+		auth_creds[uname] = passwd
+		auth_status = "User created successfully."
+	else:
+		auth_status = "Operation failed. Duplicate username."
+	pickle.dump(auth_creds, open('auth_cred.p', 'wb'))
+
+# Validate credentials from hash file in auth mode
+elif mode == hashlib.md5("auth".encode()).hexdigest():
+	if uname not in auth_creds.keys():
+		auth_status = "Operation failed. Username not found."
+	else:
+		if passwd != auth_creds[uname]:
+			auth_status = "Operation failed. Incorrect Password."
+		else:
+			auth_status = "Authentication successful."
+
+# Send auth status as response to client
+conn.send(auth_status.encode())
+
+print("Closing connection.")
+conn.close()
+server_skt.close()
